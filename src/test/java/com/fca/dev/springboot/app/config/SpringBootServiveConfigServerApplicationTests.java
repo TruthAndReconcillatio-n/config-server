0e2c92a7c6731517ff646a32edac14922ba8807f
karunsane <karunsane@gmail.com>
2020-08-01 14:01:26 -0500

    Init

---
+++ src/test/java/com/fca/dev/springboot/app/config/SpringBootServiveConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fca.dev.springboot.app.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootServiveConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
